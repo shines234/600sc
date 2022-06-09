bbd791fce4106b0f64e5846df5955ab1a7b79ece
shines234 <sjones.seamus@gmail.com>
2022-06-09 16:36:47 -0400

    finish hangman problem

--- pset2/ps2_hangman.py
+++ pset2/ps2_hangman.py
@@ -19,14 +19,14 @@ def load_words():
     Depending on the size of the word list, this function may
     take a while to finish.
     """
-    print "Loading word list from file..."
+    print("Loading word list from file...")
     # inFile: file
-    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    inFile = open(WORDLIST_FILENAME, 'r')
     # line: string
     line = inFile.readline()
     # wordlist: list of strings
-    wordlist = string.split(line)
-    print "  ", len(wordlist), "words loaded."
+    wordlist = str.split(line)
+    print("  ", len(wordlist), "words loaded.")
     return wordlist
 
 def choose_word(wordlist):
@@ -46,3 +46,37 @@ def choose_word(wordlist):
 wordlist = load_words()
 
 # your code begins here!
+def main():
+    wordlist = load_words()
+    word = choose_word(wordlist)
+    wordlen = len(word)
+    print(f"Welcome to the game, Hangman!\nI am thinking of a word that is {wordlen} letters long.")
+    print('----------')
+    nguess = 8
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    hidden_word = ''
+    for i in range(len(word)):
+        hidden_word += '_'
+    while(nguess > 0):
+        print(f'You have {nguess} guesses left')
+        print(f'Available letters {letters}')
+        guess = input("Please guess a letter: ")
+        if guess in word:
+            for ind,let in enumerate(word):
+                if guess == word[ind]:
+                    hidden_word = hidden_word[:ind] + guess + hidden_word[ind+1:]
+            letters.replace(guess,'')
+            print(f'Good guess: {hidden_word}')
+        else:
+            nguess -= 1
+            print(f'Oops! That letter is not in my word: {hidden_word}')
+    if '_' in hidden_word:
+        print('You Lose')
+        print(word)
+    else: 
+        print('Congratulations, you win')
+
+
+
+if __name__ == '__main__':
+    main()
